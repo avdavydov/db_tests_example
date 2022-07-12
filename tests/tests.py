3fff6995acd161c762f7e3c65509c9f79d0dc203
Aleksandr Davydov <av.davydof@gmail.com>
2022-07-12 12:21:57 +0700

    Init

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 import pytest
 
-from processing.bookings import get_premium_psg_list
 from processing.airline import set_flight_status, get_flight_status
+from processing.bookings import get_premium_psg_list
 from .db_test import TestBases
 
 
@@ -12,21 +12,23 @@ def test_db():
     test_base.db.stop()
 
 
-
 def test_get_premium_psg_list(test_db):
     assert len(get_premium_psg_list(limit=10000)) == 4
+
 
 def test_get_flight_status_before(test_db):
     assert get_flight_status(flight_id=33043) == [['On Time']]
 
+
 def test_set_flight_status(test_db):
     assert set_flight_status(flight_id=33043, status='Delayed') == 1
+
 
 def test_get_flight_status_after(test_db):
     assert get_flight_status(flight_id=33043) == [['Delayed']]
 
 # функция, для случая если нужно оставить активным докер-контейнер после завершения работы тестов
-def test_debug(test_db):
-    while True:
-        a = 1
-    assert 1 == 2
+# def test_debug(test_db):
+#     while True:
+#         a = 1
+#     assert 1 == 2
